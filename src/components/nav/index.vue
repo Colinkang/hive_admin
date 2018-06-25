1bb8e3b89aa45d97d4be98bd72e589ec23aad4cf
huangzhimao <zhimao.huang@evenhidata.com>
2018-06-25 09:54:22 +0800

    .

--- src/components/nav/index.vue
+++ src/components/nav/index.vue
@@ -1,44 +1,44 @@
 <template>
 <div class="nav">
   <el-menu :default-active="index" class="el-menu-vertical-demo" @select="select" @open="handleOpen" @close="handleClose"  text-color="rgba(105, 105, 106)" active-text-color="rgb(105, 105, 106)" :collapse="collapse" :router="true">
-    <el-menu-item  index="0" route="/beebox" v-show="right.indexOf('4')>-1||right.indexOf('9')>-1" :style="{'background':index==0?selectColors[index]:''}">
-      <span class="nav-text">
+    <el-menu-item  index="0" route="/beebox" v-show="right.indexOf('4')>-1||right.indexOf('9')>-1" >
+      <span class="nav-text" :style="{'background':index==0?selectColors[index]:''}">
         <i class="iconfont icon-boxxiangzi"></i>
         <span slot="title">蜂箱</span>
       </span>
     </el-menu-item>
-    <el-menu-item index="1" route="/beefarmer" v-show="right.indexOf('3')>-1||right.indexOf('8')>-1" :style="{'background':index==1?selectColors[index]:''}">
-      <span class="nav-text">
+    <el-menu-item index="1" route="/beefarmer" v-show="right.indexOf('3')>-1||right.indexOf('8')>-1" >
+      <span class="nav-text" :style="{'background':index==1?selectColors[index]:''}">
         <i class="iconfont icon-wo"></i>
         <span slot="title">蜂农</span>
       </span>
     </el-menu-item>
-    <el-menu-item index="2" route="/organization" v-show="right.indexOf('2')>-1||right.indexOf('7')>-1" :style="{'background':index==2?selectColors[index]:''}">
-      <span class="nav-text">
+    <el-menu-item index="2" route="/organization" v-show="right.indexOf('2')>-1||right.indexOf('7')>-1" >
+      <span class="nav-text" :style="{'background':index==2?selectColors[index]:''}">
         <i class="iconfont icon-renshu"></i>
         <span slot="title">组织</span>
       </span>
     </el-menu-item>
-    <el-menu-item index="3" route="/event"  v-show="right.indexOf('5')>-1||right.indexOf('10')>-1" :style="{'background':index==3?selectColors[index]:''}">
-      <span class="nav-text">
+    <el-menu-item index="3" route="/event"  v-show="right.indexOf('5')>-1||right.indexOf('10')>-1" >
+      <span class="nav-text" :style="{'background':index==3?selectColors[index]:''}">
         <i class="iconfont icon-event"></i>
         <span slot="title">事件</span>
       </span>
     </el-menu-item>
-    <el-menu-item index="4" route="/ai" :style="{'background':index==4?selectColors[index]:''}">
-      <span class="nav-text">
+    <el-menu-item index="4" route="/ai" >
+      <span class="nav-text" :style="{'background':index==4?selectColors[index]:''}">
         <i class="iconfont icon-rengongzhineng"></i>
         <span slot="title">AI</span>
       </span>
     </el-menu-item>
-    <el-menu-item index="5" route="/dron" :style="{'background':index==5?selectColors[index]:''}">
-      <span class="nav-text">
+    <el-menu-item index="5" route="/dron" >
+      <span class="nav-text" :style="{'background':index==5?selectColors[index]:''}">
         <i class="iconfont icon-UAV"></i>
         <span slot="title">无人机</span>
       </span>
     </el-menu-item>
-    <el-menu-item index="6" route="/control" v-if="right.indexOf('1')>-1||right.indexOf('6')>-1" :style="{'background':index==6?selectColors[index]:''}">
-      <span class="nav-text">
+    <el-menu-item index="6" route="/control" v-if="right.indexOf('1')>-1||right.indexOf('6')>-1" >
+      <span class="nav-text" :style="{'background':index==6?selectColors[index]:''}">
         <i class="iconfont icon-guanli2"></i>
         <span slot="title">管理</span>
       </span>
@@ -109,6 +109,10 @@ export default {
 	z-index: 1111;
 }
 
+.el-menu-item{
+  padding:0!important;
+}
+
 .el-menu-vertical-demo:not(.el-menu--collapse) {
 	width: 200px;
 	min-height: 900px;
@@ -132,12 +136,60 @@ export default {
 	height: 50px;
 	line-height: 50px;
 }
-.is-active::after {
+.is-active-0::after {
 	content: '';
 	position: absolute;
 	width: 100%;
 	height: 100%;
 	background: #769e21;
+	left: 0;
+}
+.is-active-1::after {
+	content: '';
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	background: #cb7e00;
+	left: 0;
+}
+.is-active-2::after {
+	content: '';
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	background: #42567f;
+	left: 0;
+}
+.is-active-3::after {
+	content: '';
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	background: #bc0046;
+	left: 0;
+}
+.is-active-4::after {
+	content: '';
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	background: #007eb5;
+	left: 0;
+}
+.is-active-5::after {
+	content: '';
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	background: #796a55;
+	left: 0;
+}
+.is-active-6::after {
+	content: '';
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	background: #7e308d;
 	left: 0;
 }
 
@@ -149,6 +201,10 @@ export default {
 
 .nav-text {
 	color: white;
+  display: inline-block;
+  width: 100%;
+  height: 100%;
+  text-indent: 20px;
 }
 /* .nnnnnn{
   background: #aaa !important;
